a127de58df572caa3a915187c23e581b54b3fddc
Rahul Roy <rahul.roy@sprinklr.com>
2024-10-27 07:13:16 +0530

    vercel setup

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { graphqlHTTP } = require("express-graphql");
 const cors = require("cors");
 
 const app = express();
-
 const schema = require("./schema");
 
 // allow cross origin requests
@@ -12,6 +11,15 @@ app.use(cors());
 // graphql set up
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
-app.listen(4100, () => {
-	console.log("Listening at port 4100");
-});
+// Use the PORT environment variable
+const PORT = process.env.PORT || 4100;
+
+// This console.log is only for local testing
+if (process.env.NODE_ENV !== "production") {
+	app.listen(PORT, () => {
+		console.log(`Listening at port ${PORT}`);
+	});
+}
+
+// Export the app for Vercel
+module.exports = app;
